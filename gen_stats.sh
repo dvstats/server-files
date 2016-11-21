d5983a0e92ed1f2c6e76a27ea5e5e604ab9cf356
cp2141 <cp2141@gmail.com>
2016-11-20 22:31:23 -0500

    Added fonts and completed page generation script.

--- gen_stats.sh
+++ gen_stats.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
-cd output
 
+SFDIR=/home/breakthrough/git-repos/dvstats-server-files
 
-SFDIR=..
-pisg --configfile=$SFDIR/config/pisg.conf --cfg CssDir=$SFDIR/template/ --cfg PageHead=$SFDIR/template/header.html
+cd $SFDIR/output
 
+pisg \
+    --configfile=$SFDIR/config/pisg.conf            \
+    --cfg CssDir=$SFDIR/template/                   \
+    --cfg PageHead=$SFDIR/template/header.html      \
+    --cfg PageFoot=$SFDIR/template/footer.html
+
+sed -e "5r $SFDIR/template/fonts.html" -i $SFDIR/output/dvstats.html
 
